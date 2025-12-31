fe850d80cc202a03161f12991e9aa996d661b7d7
Your Name <you@example.com>
2025-12-31 10:40:11 +0000

    実装とテストを行う

--- example/test.py
+++ example/test.py
@@ -1,14 +1,24 @@
+import sys, os
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import weakref
 import cupy as np
+from example.stage2.stage2 import *
+import contextlib
 
-# 1. オブジェクト作成
-a = np.array([1, 2, 3])
-b = weakref.ref(a)
-print(b)
-print(b())
-# 2. 参照を削除
-a = None
+x0 = Variable(np.array(1.0))
+x1 = Variable(np.array(2.0))
+t = add(x0, x1)
+y = add(x0, t)
+y.backward()
+print(x0.grad, x1.grad)
 
-# 3. 確認
-print(b)  # <weakref at ...; dead> と表示されれば成功
-print(b()) # None と表示されれば、循環参照なく解放されています
+
+x = Variable(np.array(1.0))
+y = square(x)
+y.backward()
+print(x.grad)
+
+with no_grad():
+    x = Variable(np.array(1.0))
+    y = square(x)
+    print(y.grad)
