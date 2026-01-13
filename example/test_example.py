83f938dd55ea04a7ba2fddb826857a44970b211c
Your Name <you@example.com>
2026-01-13 09:33:20 +0000

    ローゼンブルック関数の最小値を勾配降下法で求める実験

--- example/test_example.py
+++ example/test_example.py
@@ -9,10 +9,18 @@ from dezero.util import *
 
 x0 = Variable(np.array(0.0))
 x1 = Variable(np.array(0.0))
+lr = 0.001
+iters = 50000
+for i in range(iters):
+    print(x0, x1)
 
-z = rosenbrock(x0, x1)
-print(z.data)
-z.backward()
-print(x0.grad, x1.grad)
+    z = rosenbrock(x0, x1)
+
+    x0.crearngrad()
+    x1.crearngrad()
+    z.backward()
+
+    x0.data -= lr * x0.grad
+    x1.data -= lr * x1.grad
 
 plot_dot_graph(z, to_file='rosenbrock.png')
