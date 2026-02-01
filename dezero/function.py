8f4439650d5981d083d7218433be38def6edb0f3
Your Name <you@example.com>
2026-02-01 12:11:56 +0000

    Sumクラスの実装

--- dezero/function.py
+++ dezero/function.py
@@ -122,8 +122,27 @@ class BroadcastTo(Function):
     def backward(self, gy):
         gx = sum_to(gy, self.x_shape)
         return gx
+    
 def broadcast_to(x, shape):
     if x.shape == shape:
        return as_variable(x)
     return BroadcastTo(shape)(x)
+
+class SumTo(Function):
+    def __init__(self, shape):
+        self.shape = shape
+    
+    def forward(self, x):
+        self.x_shape = x.shape
+        y = util.sum_to(x, self.shape)
+        return y
+    
+    def backward(self, gy):
+        gx = broadcast_to(gy, self.x_shape)
+        return gx
+    
+def sum_to(x, shape):
+    if x.shape == shape:
+       return as_variable(x)
+    return SumTo(shape)(x)
 
