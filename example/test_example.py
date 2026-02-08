cdcfee34ab07dc5e9120f6db7cfa029ca10845bb
Your Name <you@example.com>
2026-02-08 02:54:20 +0000

    線型回帰テスト用のデータを用意

--- example/test_example.py
+++ example/test_example.py
@@ -3,11 +3,11 @@ sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import weakref
 import numpy as np
 import dezero as F
-x0 = F.Variable(np.array([[[4, 5, 6], [5, 6, 7], [7, 8, 9]]]))
-print(x0,"\n")
-x1 = x0.T
-y = F.cos(x1)
-y = y.backward(create_graph=True)
-print(x0.grad.reshape(9,1))
-x2 = x0.transpose((1, 2, 0))
-print (x2.data)
+
+np.random.seed(0)
+x = np.random.randn(100, 1)
+y = 5 + 2 * x + np.random.randn(100, 1)
+
+W = F.Variable(np.zeros(1, 1))
+b = F.Variable(np.zeros(1))
+
